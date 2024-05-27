a02fe7f24b210bade69223b80bbd5a74d488996c
Faizhar <mohamad.faizhar@gmail.com>
2024-05-27 18:49:15 +0700

    feat: landing page

---
+++ src/__mock/productList.ts
@@ -0,0 +1,82 @@
+export const PRODUCT_LIST = [
+  {
+    imageUrl:
+      "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcSAPYXXfb_ubIDZWWp9s_4lJ9LRMoPnD5pP9WCwDeGNrZ-3OZh9pG5WeEmf5uNF",
+    title: "Sunflower Sunshine",
+    description:
+      "A vibrant bouquet bursting with sunshine-colored sunflowers, guaranteed to brighten any day.",
+    price: "$39.99",
+  },
+  {
+    imageUrl:
+      "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcRFCXLNeSaKQdG95tFvQBwZM0ultCc-0ungay3I285wcfvp6HkRvGuTmfzXGCCw",
+    title: "Pastel Dream",
+    description:
+      "Delicate and delightful, this pastel bouquet features soft pinks, purples, and blues for a touch of ethereal beauty.",
+    price: "$44.99",
+  },
+  {
+    imageUrl:
+      "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcSi6PnnvmfD3Ml0S044LOg_SdxLBvw0MPbr65npW_dZlnutJKe6n34tK6ZznXpb",
+    title: "Romantic Roses",
+    description:
+      "The timeless symbol of love, a classic bouquet of red roses speaks volumes.",
+    price: "$59.99",
+  },
+  {
+    imageUrl:
+      "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcTTEhwPvhHONde4Blux0ZiD0P0rAX2G0QsBKCJOnCSh14v6vwYz4SLFOk8R7tVh",
+    title: "Exotic Paradise",
+    description:
+      "Transport yourself to a tropical paradise with this vibrant bouquet featuring exotic blooms in stunning colors.",
+    price: "$49.99",
+  },
+  {
+    imageUrl:
+      "https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcSMZZkhPJCsfCYNAmVFSohEGUEh3gy86J4o1_7SojgsklAJae4vClHgBxgdFkG3",
+    title: "Lily Serenity",
+    description:
+      "Pure elegance personified, this bouquet of white lilies exudes peace and tranquility.",
+    price: "$42.99",
+  },
+  {
+    imageUrl:
+      "https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcSO7sx9GttUq-a2mhGlMJxvk9aEoeqs2--HkzkkOse1sTwur9wMcM6U6WWpq4Ri",
+    title: "Wildflower Meadow",
+    description:
+      "Capture the essence of a summer meadow with this charming bouquet of colorful wildflowers.",
+    price: "$34.99",
+  },
+  {
+    imageUrl:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRFvzojM_LMigzHO9bnGtlVlR9kg9Ek22hdDBNVukRGYpOfFmRR3rQHXOlFnHa6",
+    title: "Monochrome Magic",
+    description:
+      "A sophisticated statement piece, this bouquet features a stunning combination of black and white blooms.",
+    price: "$47.99",
+  },
+  {
+    imageUrl:
+      "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcS_EODka6gb_OdXPvxI1r-82CyJ-MNuwnaoHr-dMkvHsKQZJ7zLoYTLKSy55Gqy",
+    title: "Succulent Symphony",
+    description:
+      "A unique and low-maintenance option, this succulent bouquet adds a touch of modern style to any space.",
+    price: "$32.99",
+  },
+  // {
+  //   imageUrl:
+  //     "https://www.weddingflora.com/blog/2019/3/11/what-exactly-are-seasonal-flowers-xsjwc",
+  //   title: "Seasonal Surprise",
+  //   description:
+  //     "Be delighted by nature's bounty with a bouquet featuring the freshest seasonal blooms.",
+  //   price: "$49.99 (Price can vary depending on season)",
+  // },
+  // {
+  //   imageUrl:
+  //     "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcSrg7xa5cLGJKr_-NqErqlRSb1A4_xN34OydG-uuzRvh7vFncsX0YjJuyL7oDTV",
+  //   title: "DIY Bouquet Kit",
+  //   description:
+  //     "Unleash your creativity with a DIY bouquet kit, perfect for a fun and personalized gift.",
+  //   price: "$39.99",
+  // },
+];
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,26 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["ClashDisplay-Variable", ...defaultTheme.fontFamily.sans],
+        paragraph: ["CabinetGrotesk-Variable", ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        "main-pink": "#DF8DC8",
+        "main-green": "#357269",
+        "main-white": "#F5F5F5",
+        "main-black": "#2c2c2c",
+      },
+      backgroundImage: {
+        bespoke: "url(./src/assets/background-bespoke.png)",
+      },
+      boxShadow: {
+        "overlay-image": "inset 0 0 0 1000px rgba(0,0,0,.6)",
+      },
+    },
+  },
+  plugins: ["prettier-plugin-tailwindcss"],
+};
